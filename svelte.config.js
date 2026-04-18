75b296402db89ba158a90bf49d6b9d978e35479f
znotfireman <znotfireman@gmail.com>
2026-04-18 18:08:24 +0700

    fix(ci): blank base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+    kit: {
+        adapter: adapter(),
+        paths: {
+            base: ""
+        }
+    }
+};
 
 export default config;
